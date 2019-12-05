47844b122526578009caed5911838c7c19a91c74
zompi <zompi2@gmail.com>
2019-12-05 20:45:39 +0100

    Result of Day1

--- Day0/main.cpp
+++ Day0/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <fstream>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int CalcFuel(const int iMass)
+{
+    const int result = iMass / 3 - 2;
+    return result <= 0 ? 0 : result + CalcFuel(result);
+}
+
+int main() 
+{
+    std::ifstream infile("input.txt");
+    int x, acc = 0;
+    while (infile >> x)
+    {
+       acc += CalcFuel(x);
+    }
+
+    std::cout << "Result: " << acc << '\n';
 
     {
         std::cout << "Press any key to continue...\n";
