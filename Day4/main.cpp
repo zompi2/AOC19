859491b5e0e84b49713e52bead02d20ab1a9966d
zompi <zompi2@gmail.com>
2019-12-07 14:17:48 +0100

    Result for Day4.

--- Day4/main.cpp
+++ Day4/main.cpp
@@ -4,22 +4,124 @@
 #include <sstream>
 #include <iterator>
 #include <assert.h>
+#include <algorithm>
 
-std::vector<std::string> Split(const std::string& s, char delimiter)
+void CollectDigits(std::vector<int>& outDigits, int inNumber) 
 {
-   std::vector<std::string> tokens;
-   std::string token;
-   std::istringstream tokenStream(s);
-   while (std::getline(tokenStream, token, delimiter))
-   {
-      tokens.push_back(token);
-   }
-   return tokens;
+    if (inNumber > 9) 
+    {
+        CollectDigits(outDigits, inNumber / 10);
+    }
+    outDigits.push_back(inNumber % 10);
 }
+
+class Number
+{
+    int Digits[6];
+ 
+ public:
+    Number(const int InitNumber)
+    {
+        std::vector<int> TempDigits;
+        CollectDigits(TempDigits, InitNumber);
+        assert(TempDigits.size() == 6);
+
+        std::copy(TempDigits.begin(), TempDigits.end(), Digits);
+    }
+
+    void Increase()
+    {
+        for (int i = 5; i >= 0; i--)
+        {
+            if (Digits[i] == 9)
+            {
+                Digits[i] = 0;
+            }
+            else
+            {
+                Digits[i]++;
+                break;
+            }
+        }
+    }
+
+    void Print()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            std::cout << Digits[i];
+        }
+        std::cout << '\n';
+    }
+
+    bool Check()
+    {
+        bool bFoundPair = false;
+        bool bFoundPairInPreviousCheck = false;
+        bool bFoundPairChecked = false;
+
+        for (int i=1; i<6; i++)
+        {
+            if (Digits[i] < Digits[i-1])
+            {
+                // next number is lesser. Failed.
+                return false;
+            }
+
+            if (Digits[i] == Digits[i-1])
+            {
+                if (bFoundPairInPreviousCheck)
+                {
+                    // Pair was found in previous check it means it is a triplet or more.
+                    // This is not a pair!
+                    bFoundPair = false;
+                }
+                else
+                {
+                    // found pair
+                    bFoundPair = true;
+                    bFoundPairInPreviousCheck = true;
+                    if (i == 5)
+                    {
+                        // this is the last digit and it is a pair.
+                        bFoundPairChecked = true;
+                    }
+                }
+            }
+            else
+            {
+                if (bFoundPair)
+                {
+                    // There was a pair previously, which means we have at least one pair!
+                    bFoundPairChecked = true;
+                }
+                bFoundPairInPreviousCheck = false;
+            }
+        }
+
+        return bFoundPairChecked;
+    }
+};
 
 int main() 
 {
-    std::ifstream infile("input.txt");
+
+    const int Begin = 273025;
+    const int End = 767253;
+
+    int PasswordsFound = 0;
+
+    Number number(Begin);
+    for (int i = Begin; i <= End; i++)
+    {
+        if (number.Check())
+        {
+            PasswordsFound++;
+        }
+        number.Increase();
+    }
+
+    std::cout << "Result: " << PasswordsFound << " \n";
 
     return 0;
 }
