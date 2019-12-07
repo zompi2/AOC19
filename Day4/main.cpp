1d9818617d98c7ef9209b2a92743dd341b051d6f
zompi <zompi2@gmail.com>
2019-12-07 13:37:39 +0100

    day4 init

---
+++ Day4/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iterator>
+#include <assert.h>
+
+std::vector<std::string> Split(const std::string& s, char delimiter)
+{
+   std::vector<std::string> tokens;
+   std::string token;
+   std::istringstream tokenStream(s);
+   while (std::getline(tokenStream, token, delimiter))
+   {
+      tokens.push_back(token);
+   }
+   return tokens;
+}
+
+int main() 
+{
+    std::ifstream infile("input.txt");
+
+    return 0;
+}
