3ec0d79ef3816aa22e9470385d13ce7e067eed1a
zompi <zompi2@gmail.com>
2019-12-09 21:21:47 +0100

    Day5 done.

--- Day5/main.cpp
+++ Day5/main.cpp
@@ -20,7 +20,87 @@ std::vector<std::string> Split(const std::string& s, char delimiter)
 
 int main() 
 {
-    std::ifstream infile("input.txt");
+   std::ifstream infile("input.txt");
+   
+   std::vector<int> data;
+   std::string value;
+   while (std::getline(infile, value, ','))
+   {
+      data.push_back(std::stoi(value));
+   }
 
-    return 0;
+   bool bFinished = false;
+   int idx = 0;
+
+   while (bFinished == false)
+   {
+      const int OPCode = data[idx] % 100;
+      int immediateMode = data[idx] / 100;
+
+      const int A = immediateMode / 100; immediateMode -= A*100;
+      const int B = immediateMode / 10; immediateMode -= B*10;
+      const int C = immediateMode;
+
+      const int val1 = C == 0 ? data[data[idx+1]] : data[idx+1];
+      const int val2 = B == 0 ? data[data[idx+2]] : data[idx+2];
+
+      int& result =  data[data[idx+3]];
+
+      if (OPCode == 99)
+      {
+         bFinished = true;
+         break;
+      }
+      else if (OPCode == 1 || OPCode == 2)
+      {
+         if (OPCode == 1)
+         {
+            result = val1 + val2;
+         }
+         else if (OPCode == 2)
+         {
+            result = val1 * val2;
+         }
+         idx += 4;
+      }
+      else if (OPCode == 3)
+      {
+         std::cout << "Please give input: ";        
+         std::cin >> data[data[idx+1]];
+         std::cin.ignore();
+         std::cout << '\n';
+         idx += 2;
+      }
+      else if (OPCode == 4)
+      {
+         const int val = C == 0 ? data[data[idx+1]] : data[idx+1];
+         std::cout << "Output: " << val << '\n';
+         idx += 2;
+      }
+      else if (OPCode == 5 || OPCode == 6)
+      {
+         if ((OPCode == 5 && val1 != 0) || (OPCode == 6 && val1 == 0))
+         {
+            idx = val2;
+         }
+         else
+         {
+            idx += 3;
+         }
+      }
+      else if (OPCode == 7 || OPCode == 8)
+      {
+         if (OPCode == 7)
+         {
+            result = (val1 < val2) ? 1 : 0;
+         }
+         else if (OPCode == 8)
+         {
+            result = (val1 == val2) ? 1 : 0;
+         }
+         idx += 4;
+      }
+   } 
+
+   return 0;
 }
