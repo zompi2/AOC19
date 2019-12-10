c9deb87536185f9020d6e35f226d9700839b6155
zompi <zompi2@gmail.com>
2019-12-10 22:07:16 +0100

    Prepared for day10

---
+++ Day10/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iterator>
+#include <assert.h>
+#include <set>
+#include <memory>
+#include <map>
+#include <algorithm>
+#include <functional>
+#include <array>
+
+int main() 
+{
+   std::ifstream infile("input.txt");   
+   
+
+   
+   return 0;
+}
