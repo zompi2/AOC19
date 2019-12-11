ed50a4773ffe9f7ed8cfb9e475ff7378f20873ff
zompi <zompi2@gmail.com>
2019-12-11 21:36:32 +0100

    Day 11 prepare

---
+++ Day11/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iterator>
+#include <assert.h>
+#include <set>
+#include <memory>
+#include <map>
+#include <algorithm>
+#include <functional>
+#include <array>
+#include <cmath>
+
+int main() 
+{
+   std::ifstream infile("input.txt");   
+ 
+   return 0;
+}
