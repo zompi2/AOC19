f8a549155cd45b7c0a6560176cd67439c0a206b8
zompi <zompi2@gmail.com>
2019-12-12 23:16:56 +0100

    almos

--- Day10/main.cpp
+++ Day10/main.cpp
@@ -11,6 +11,7 @@
 #include <functional>
 #include <array>
 #include <cmath>
+#include <math.h>
 
 const float EPSILON = .0001f;
 
@@ -19,25 +20,56 @@ float AlmostEqual(float a, float b)
    return std::fabs(a-b) <= EPSILON;
 }
 
-class Asteroid
+class Point
 {
 public:
-
    float x = 0.f;
    float y = 0.f;
-   int detected = 0;
 
-   Asteroid(float inX, float inY) :
+   Point() 
+   {}
+
+   Point(float inX, float inY) :
       x(inX), y(inY)
    {}
 
-   Asteroid()
-   {}
-
-   bool operator==(const Asteroid& other)
+   bool operator==(const Point& other) const
    {
       return (AlmostEqual(x, other.x) && AlmostEqual(y, other.y));
    }
+};
+
+class Vec2D : public Point
+{
+public:
+   Vec2D(float inX, float inY) : 
+      Point(inX, inY)
+   {}
+
+   float Length() const
+   {
+      return std::sqrt(std::pow(x, 2) + std::pow(y, 2));
+   }
+
+   Vec2D Normalized() const
+   {
+      const float len = Length();
+      return Vec2D(x / len, y / len);
+   }
+};
+
+class Asteroid : public Point
+{
+public:
+   int detected = 0;
+
+   Asteroid() : 
+      Point()
+   {}
+
+   Asteroid(float inX, float inY) : 
+      Point(inX, inY)
+   {}
 };
 
 class Line
@@ -53,38 +85,38 @@ public:
 
    Line() {}
 
-   Line(const Asteroid& a1, const Asteroid& a2)
+   Line(const Point& p1, const Point& p2)
    {
-      if (AlmostEqual(a1.x, a2.x))
+      if (AlmostEqual(p1.x, p2.x))
       {
          isVertical = true;
-         x = a1.x;
+         x = p1.x;
       }
-      else if (AlmostEqual(a1.y, a2.y))
+      else if (AlmostEqual(p1.y, p2.y))
       {
          isHorizontal = true;
-         y = a1.y;
+         y = p1.y;
       }
       else
       {
-         a = (a2.y - a1.y) / (a2.x - a1.x);
-         b = a1.y - a*a1.x;
+         a = (p2.y - p1.y) / (p2.x - p1.x);
+         b = p1.y - a*p1.x;
       }
    }
 
-   bool IsOnLine(const Asteroid& a1)
+   bool IsOnLine(const Point& p1)
    {
       if (isVertical)
       {
-         return AlmostEqual(a1.x, x);
+         return AlmostEqual(p1.x, x);
       }
       else if (isHorizontal)
       {
-         return AlmostEqual(a1.y, y);
+         return AlmostEqual(p1.y, y);
       }
       else
       {
-         return AlmostEqual(a1.y, a*a1.x+b);
+         return AlmostEqual(p1.y, a*p1.x+b);
       }
    }
 
@@ -103,10 +135,10 @@ public:
 
 bool IsBetween(float x, float a, float b)
 {
-   return ((a < x && x < b) || (b < x && x < a)); 
+   return ((a <= x && x <= b) || (b <= x && x <= a)); 
 }
 
-bool IsBetweenAsteroid(const Asteroid& x, const Asteroid& a, const Asteroid& b)
+bool IsBetweenPoints(const Point& x, const Point& a, const Point& b)
 {
    return IsBetween(x.x, a.x, b.x) && IsBetween(x.y, a.y, b.y);
 }
@@ -116,12 +148,100 @@ std::string CoordToName(int x, int y)
    return std::to_string(x) + 'x' + std::to_string(y);
 }
 
+inline Vec2D FromTwoPoints(const Point& p1, const Point& p2)
+{
+   return Vec2D(p2.x-p1.x, p2.y - p1.y);
+}
+
+inline float Dot(const Vec2D& v1, const Vec2D& v2)
+{
+   return v1.x*v2.x + v1.y*v2.y;
+}
+
+inline float ToDegrees(float rad)
+{
+   return rad * (180.f / M_PI);
+}
+
+class Ray
+{
+public:
+
+   Point p1, p2;
+   Line line;
+   float angle = 0;
+   std::map<int, Asteroid*> asteroidsOnLine;
+   std::map<int, Asteroid*>::iterator asteroidsIt;
+
+   Ray() {}
+
+   Ray(const Point& inP1, const Point& inP2) :
+      p1(inP1), p2(inP2), line(inP1, inP2)
+   {
+      Vec2D vec1 = FromTwoPoints(p1, Point(p1.x ,0));
+      Vec2D vec2 = FromTwoPoints(p1, p2);
+      angle = ToDegrees(std::acos(Dot(vec1.Normalized(), vec2.Normalized())));
+      if (angle > 0 && angle < 180)
+      {
+          if (vec2.x < vec1.x)
+          {
+             angle = 360 - angle;
+          }
+      }
+      angle = roundf(angle * 100) / 100;
+   }
+
+   void CollectAllAsteroids(std::vector<Asteroid>& a1)
+   {
+      for (Asteroid& a : a1)
+      {
+         if (a == p1) continue;
+         //if (a == p2) continue;
+
+         if (line.IsOnLine(a))
+         {
+            if (line.isVertical)
+            {
+               if (IsBetween(a.y, p1.y, p2.y) == false) continue; 
+            }
+            else if (line.isHorizontal)
+            {
+               if (IsBetween(a.x, p1.x, p2.x) == false) continue;
+            }
+            else
+            {
+               if (IsBetweenPoints(a, p1, p2) == false) continue;
+            }
+
+            int distance = std::abs(a.x - p1.x) + std::abs(a.y - p1.y);
+            asteroidsOnLine[distance] = &a;
+         }
+      }
+
+      asteroidsIt = asteroidsOnLine.begin();
+   }
+
+   Asteroid* GetNextAsteroid()
+   {
+      if (asteroidsIt == asteroidsOnLine.end())
+      {
+         return nullptr;
+      }
+      else
+      {
+         Asteroid* result = (*asteroidsIt).second;
+         asteroidsIt++;
+         return result;
+      }
+   }
+};
+
 int main() 
 {
    std::ifstream infile("input.txt");   
    std::string line;
 
-   std::map<std::string, Asteroid> asteroids;
+   std::vector<Asteroid> asteroids;
 
    int y = 0;
    int x = 0;
@@ -132,7 +252,7 @@ int main()
       {
          if (c=='#')
          {
-            asteroids[CoordToName(x,y)] = Asteroid(x,y);
+            asteroids.push_back(Asteroid(x,y));
          }
          x++;
       }
@@ -142,19 +262,16 @@ int main()
    int planeW = x;
    int planeH = y;
 
-   for (auto& a1Iter : asteroids)
+   for (auto& a1 : asteroids)
    {
-      Asteroid& a1 = a1Iter.second;
-      for (auto& a2Iter : asteroids)
+      for (auto& a2 : asteroids)
       {
-         Asteroid& a2 = a2Iter.second;
          if (a1 == a2) continue;
 
          Line line(a1, a2);
          bool a2IsVisible = true;
-         for (auto& a3Iter : asteroids)
+         for (auto& a3 : asteroids)
          {
-            Asteroid& a3 = a3Iter.second;
             if (a1 == a3) continue;
             if (a2 == a3) continue;
 
@@ -170,7 +287,7 @@ int main()
                }
                else
                {
-                  if (IsBetweenAsteroid(a3, a1, a2)) {a2IsVisible = false; break;}
+                  if (IsBetweenPoints(a3, a1, a2)) {a2IsVisible = false; break;}
                }
             }
          }
@@ -184,10 +301,8 @@ int main()
    int biggestDetect = -1;
    Asteroid* abd = nullptr;
 
-   for (auto& a1Iter : asteroids)
+   for (auto& a1 : asteroids)
    {
-      Asteroid& a1 = a1Iter.second;
-     // std::cout << '(' << a1.x << ',' << a1.y << ")= " << a1.detected << '\n';
       if (a1.detected > biggestDetect)
       {
          biggestDetect = a1.detected;
@@ -199,42 +314,40 @@ int main()
       '(' << abd->x << ',' << abd->y << ")= " << abd->detected << '\n';
    
 
-   int shot = 1;
-   
-   Asteroid aTarget(abd->x, 0.f);
-   bool moveHorizontally = true;
-   while(shot <= 3)
+   std::map<float, Ray> rays;
+   for (auto& a1 : asteroids)
    {
-      Line shotLine(*abd, aTarget);
-      if (aTarget.y == 0) // moving through up edge
-      {
-         for (int y = abd->y - 1; y >=0; y--)
-         {
-            float x = shotLine.GetXFromY(y);
-            if (std::abs(x - (int)x) < 0.001)
-            {
-               const std::string id = CoordToName((int)x,(int)y);
-               auto asteroidFound = asteroids.find(id);
-               if (asteroidFound != asteroids.end())
-               {
-                  std::cout << "Veporizing: " << id << '\n';
-                  asteroids.erase(asteroidFound);
-                  break;
-               }
-            }
-         }
-         if (aTarget.x == planeW-1)
-         {
-            aTarget.y++;
-         }
-         else
-         {
-            aTarget.x++;
-         }
-      }
+      if (a1 == *abd) continue;
 
-      shot++;
+      Ray ray(*abd, a1);
+      
+      if (rays.find(ray.angle) == rays.end())
+      {
+         rays[ray.angle] = ray;
+         rays[ray.angle].CollectAllAsteroids(asteroids);
+      }
    }
 
+   bool ShouldEnd = false;
+   int i = 1;
+   while(ShouldEnd == false)
+   {
+      ShouldEnd = true;
+      for (auto& ray : rays)
+      {
+         const Asteroid* asteroid = ray.second.GetNextAsteroid();
+         if (asteroid != nullptr)
+         {
+            std::cout << i << ". Vaporized asteroid: (" << asteroid->x << ',' << asteroid->y << ")\n";          
+            if (i%9 == 0)
+            {
+               std::cout << "---\n";
+            }
+            i++;
+            ShouldEnd = false;
+         }
+      }
+   }
+   
    return 0;
 }
