663f3293a7bb9cb0405fc92d0687f4873208ab05
zompi <zompi2@gmail.com>
2019-12-15 22:40:30 +0100

    Prepared for day16

---
+++ Day16/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iterator>
+#include <assert.h>
+#include <set>
+#include <memory>
+#include <map>
+#include <algorithm>
+#include <functional>
+#include <array>
+#include <cmath>
+#include <chrono>
+#include <thread>
+
+int main() 
+{
+   std::ifstream infile("input.txt");   
+
+   return 0;
+}
