35fa913b6681d5272f0d6040a4b4facdc1f47ef3
zompi <zompi2@gmail.com>
2019-12-15 13:07:26 +0100

    Day 14 don1

--- Day14/main.cpp
+++ Day14/main.cpp
@@ -29,12 +29,12 @@ std::vector<std::string> Split(const std::string& s, char delimiter)
 class Reactor
 {
 public:
-   std::map<std::string, int> Requires;
-   std::pair<std::string, int> Produces;
+   std::map<std::string, long long> Requires;
+   std::pair<std::string, long long> Produces;
 };
 std::vector<Reactor> reactors;
-std::map<std::string, int> partsRequired;
-int oreRequired = 0;
+std::map<std::string, long long> partsRequired;
+long long oreRequired = 0;
 
 bool IsActeptingOreOnly(const std::string& name)
 {
@@ -55,7 +55,7 @@ bool IsActeptingOreOnly(const std::string& name)
    return false;
 }
 
-void AddOrInrease(std::map<std::string, int>& map, const std::string& what, int amount)
+void AddOrInrease(std::map<std::string, long long>& map, const std::string& what, long long amount)
 {
    if (amount > 0)
    {
@@ -70,41 +70,16 @@ void AddOrInrease(std::map<std::string, int>& map, const std::string& what, int 
    }
 }
 
-int main() 
+long long CalcOreRequiredForFuel(long long fuelAmount)
 {
-   std::ifstream infile("input.txt");   
-   std::string s;
-
-   // PARSE INPUT TO REACTORS
-   while(std::getline(infile, s))
-   {
-      reactors.push_back(Reactor());
-      Reactor* currentReactor = &reactors.back();
-
-      std::size_t sep = s.find(" => ");
-      std::vector<std::string> reqs = Split(s.substr(0, sep), ',');
-      for (std::string& req : reqs)
-      {
-         std::vector<std::string> reqval = Split(req, ' ');
-         if (reqval[0] == "")
-         {
-            reqval.erase(reqval.begin());
-         }
-         currentReactor->Requires[reqval[1]] = std::atoi(reqval[0].c_str());
-      }
-
-      std::vector<std::string> outs = Split(s.substr(sep + 4, s.length() - sep), ' ');
-      currentReactor->Produces.first = outs[1];
-      currentReactor->Produces.second = std::atoi(outs[0].c_str());
-   }
-
-   partsRequired["FUEL"] = 1;
+   partsRequired.clear();
+   partsRequired["FUEL"] = fuelAmount;
 
    bool bPurePartsOnly = false;
    while(bPurePartsOnly == false)
    {
       bPurePartsOnly = true;
-      std::map<std::string, int> subParts;
+      std::map<std::string, long long> subParts;
       for (auto& part : partsRequired)
       {
          if (IsActeptingOreOnly(part.first) == false)
@@ -116,7 +91,7 @@ int main()
                {
                   if (r.Produces.first == part.first)
                   {
-                     int c = (int)std::ceil((float)part.second / r.Produces.second);
+                     int c = (long long)std::ceil((double)part.second / r.Produces.second);
                      for (auto& req : r.Requires)
                      {
                         AddOrInrease(subParts, req.first, req.second * c);
@@ -143,15 +118,104 @@ int main()
       {
          if (part.first == r.Produces.first)
          {
-            int c = (int)std::ceil((float)part.second / r.Produces.second);
-            int x = c * r.Requires["ORE"];
+            long long c = (long long)std::ceil((double)part.second / r.Produces.second);
+            long long x = c * r.Requires["ORE"];
             oreRequired += c * r.Requires["ORE"];
             break;
          }
       }
    }
+
+   return oreRequired;
+}
+
+int main() 
+{
+   std::ifstream infile("input.txt");   
+   std::string s;
+
+   // PARSE INPUT TO REACTORS
+   while(std::getline(infile, s))
+   {
+      reactors.push_back(Reactor());
+      Reactor* currentReactor = &reactors.back();
+
+      std::size_t sep = s.find(" => ");
+      std::vector<std::string> reqs = Split(s.substr(0, sep), ',');
+      for (std::string& req : reqs)
+      {
+         std::vector<std::string> reqval = Split(req, ' ');
+         if (reqval[0] == "")
+         {
+            reqval.erase(reqval.begin());
+         }
+         currentReactor->Requires[reqval[1]] = std::atoll(reqval[0].c_str());
+      }
+
+      std::vector<std::string> outs = Split(s.substr(sep + 4, s.length() - sep), ' ');
+      currentReactor->Produces.first = outs[1];
+      currentReactor->Produces.second = std::atoll(outs[0].c_str());
+   }
+
+
+
+
+
+   const long long req = 1000000000000;
+   long long result = 0;
+   long long check = 1;
+   long long checkStep = 100000;
+   int checkDir = 0;
+
+   while (true)
+   {
+      long long oreRequired = CalcOreRequiredForFuel(check);
+      if (oreRequired == req)
+      {
+         result = check;
+         break;
+      }
+      else if (checkStep == 0)
+      {
+         if (oreRequired > req)
+         {
+            result = check-1;
+            break;
+         }
+         else if (oreRequired < req)
+         {
+            result = check;
+            break;
+         }
+      }
+      else
+      {
+         if (oreRequired > req)
+         {
+            if (checkDir == 1)
+            {
+               checkStep /= 2;
+            }
+
+            check -= checkStep;
+            checkDir = -1; 
+         }
+         else if (oreRequired < req)
+         {
+            if (checkDir == -1)
+            {
+               checkStep /= 2;
+            }
+
+            check += checkStep;
+            checkDir = 1;
+         }
+      }
+      
+   }
+ 
    
-   std::cout << "Ore used: " << oreRequired << '\n';
+   std::cout << "Maximum FUEL to be made: " << result << '\n';
 
    return 0;
 }
